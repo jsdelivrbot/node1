e3e51913bd1702eee4a9ef1dbed8dab92832e6fa
lkv123 <lakshmisoujanyakota@gmail.com>
2018-04-02 13:58:34 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,11 @@ var express = require('express');
 var router = express.Router();
 const pg = require('pg');
 const path = require('path');
-const connectionString = process.env.DATABASE_URL || 'postgres://postgres:SoujanyaVishnu1@localhost:5432/db1';
+const connectionString = process.env.DATABASE_URL || 'postgres://fzdbdqvcsbxxnx:642b7242b56c1be0bea176bc456db038d85e2bdbaf941a62fe0d98974a1d4ec7@ec2-54-243-63-13.compute-1.amazonaws.com:5432/drq1nrcp0o4mq';
 const config ={
-	user: 'postgres',
-	database:'db1',
-	password:'SoujanyaVishnu1',
+	user: 'fzdbdqvcsbxxnx',
+	database:'drq1nrcp0o4mq',
+	password:'642b7242b56c1be0bea176bc456db038d85e2bdbaf941a62fe0d98974a1d4ec7',
 	port:5432
 	
 };
