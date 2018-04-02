6e27503ea7062328bfd9c67995911603894be81f
lkv123 <lakshmisoujanyakota@gmail.com>
2018-04-02 14:15:47 +0530

    Update database.js

--- models/database.js
+++ models/database.js
@@ -1,5 +1,5 @@
 const pg = require('pg');
-const connectionString = process.env.DATABASE_URL || 'postgres://postgres:SoujanyaVishnu1@localhost:5432/db1';
+const connectionString = process.env.DATABASE_URL || 'postgres://fzdbdqvcsbxxnx:642b7242b56c1be0bea176bc456db038d85e2bdbaf941a62fe0d98974a1d4ec7@ec2-54-243-63-13.compute-1.amazonaws.com:5432/drq1nrcp0o4mq';
 
 const client = new pg.Client(connectionString);
 client.connect();
